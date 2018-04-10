b9ecde6494c7fa37de8e2f52fc1e7a5a10216675
Covalence Projects <paytonhaisten@gmail.com>
2018-04-09 19:52:35 -0500

    About Me section added, new image added, styling added for about me section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,4 @@
 import React, { Component } from 'react';
-// import image from '../images/jumbo.jpg';
-
-// const jumbotron = {
-//     backgroundImage: "image",
-
-// };
 
 export default class Home extends Component {
 	render() {
@@ -15,6 +9,23 @@ export default class Home extends Component {
 						<p className="headerText">Payton</p>
 						<p className="headerText">Haisten</p>
 						<p className="underText">Full Stack Web Developer</p>
+					</div>
+				</div>
+				<div className="container" id="body">
+					<div className="row" id="about-section">
+						<div className="col" id="about-image" />
+						<div className="col">
+							<h1 className="row ml-2" id="about-header">
+								About Me
+							</h1>
+							<p className="row ml-2" id="about-text">
+								Web Developer with experience in creating responsive websites
+								and applications, that are aesthetically pleasing and
+								functional. With my experience in operations and training I am
+								able to learn in fast pace environments, improve processes and
+								efficiencies, and lead others with deadlines in mind.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
